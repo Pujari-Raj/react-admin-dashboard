5a28603badc2b587be84546febb4eeaa45c48f10
Pujari-Raj <basvarajpujari607@gmail.com>
2023-04-22 13:34:38 +0530

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Navbar from "./components/Navbar/navbar";
+import Sidebar from "./components/Sidebar/sidebar";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/> 
+      <div className="container">
+        <Sidebar/>
+        <div className="other">other page</div>
+      </div>
     </div>
   );
 }
