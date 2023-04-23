df7f9d010db3c14756502ec4105e7800e5b38a19
Pujari-Raj <basvarajpujari607@gmail.com>
2023-04-23 20:58:48 +0530

    added rechart-linechart in home pagw

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './Home.css';
 // import  FeaturedInfo  from "./components/FeaturedInfo/FeaturedInfo";
 import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
-
+import Chart from "../../components/Charts/Chart";
+import { userData } from "../../Dummydata";
 
 const Home = () => {
   return (
     <div className='home'>
-      <FeaturedInfo/>
+      <FeaturedInfo />
+      <Chart data={userData} title="Sales Analytics" grid dataKey="Active User"  />
     </div>
   )
 }
 
-export default Home
+export default Home;
---
+++ src/components/Charts/Chart.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Chart.css';
+import {
+    LineChart,
+    Line,
+    XAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+    Legend,
+    YAxis,
+} from "recharts";
+  
+
+const Chart = ({ title, data, dataKey, grid }) => {
+    
+  return (
+    <div className="chart">
+        <h3 className="chartTitle">{title}</h3>
+        <ResponsiveContainer width="100%" aspect={4/1}>
+            <LineChart data={data}>
+                <XAxis dataKey="name" stroke="#19191e" />
+                {/* <YAxis /> */}
+                <Line type="monotone" dataKey={dataKey} stroke="#323239" />
+                <Tooltip/>
+                <CartesianGrid  stroke="#e0dfdf" strokeDasharray="5 5"  />
+                <Legend/>
+            </LineChart>
+        </ResponsiveContainer>
+    </div>
+  )
+}
+
+export default Chart
