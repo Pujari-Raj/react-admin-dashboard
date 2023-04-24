e3c4530c3660f74b364f6104f15868607cae2482
Pujari-Raj <basvarajpujari607@gmail.com>
2023-04-24 13:43:26 +0530

    fixed routes error

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import Navbar from "./components/Navbar/navbar";
 import Sidebar from "./components/Sidebar/sidebar";
 import './App.css';
 import Home from "./Pages/Home/Home";
+import UsersList from "./Pages/UsersList/UsersList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/> 
-      <div className="container">
-        <Sidebar/>
-        <Home/>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Sidebar />
+          {/* <Home/> */}
+          <Routes>
+            <Route path='/' exact element={<Home />}></Route>
+            <Route  path='/users' exact element={<UsersList />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/UsersList/UsersList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './UsersList.css';
+
+const UsersList = () => {
+  return (
+    <div className="userslist">UsersList</div>
+  )
+}
+
+export default UsersList;
